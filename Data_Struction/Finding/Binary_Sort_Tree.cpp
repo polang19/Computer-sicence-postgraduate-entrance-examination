c83276df87c7302e5716a963decf963d720ab6b0
polang <caoxin4396@gmail.com>
2023-05-12 22:31:52 +0800

    Update

--- Data_Struction/Finding/Binary_Sort_Tree.cpp
+++ Data_Struction/Finding/Binary_Sort_Tree.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 
 using namespace std;
-
+//Define Node
 struct Node{
     int key;
     Node* left;
     Node* right;
     Node(int x) : key(x),left(NULL),right(NULL){}
 };
+//Search Node by key
 Node* Search(Node* root,int key){
     if (root == NULL || root->key == key){
         return root;
@@ -19,15 +20,18 @@ Node* Search(Node* root,int key){
         Search(root->right,key);
     }
 }
+//serve for Delete Function
 Node* FindMin(Node* root){
     while (root->left != NULL){
         root = root->left;
     }
     return root;
 }
+//Insert Node
 void Insert(Node* &root,int key){
     if (root == NULL){
-        root  =  new  Node(key);    //若为空，则新建节点
+        //if empty,create New node
+        root  =  new  Node(key);    
         return;
     }
     if (key < root->key){
@@ -37,6 +41,7 @@ void Insert(Node* &root,int key){
         Insert(root->right,key);
     }
 }
+//Delete Node
 Node* Delete(Node* &root,int key){
     if (root == NULL){
         return root;
@@ -48,24 +53,30 @@ Node* Delete(Node* &root,int key){
         root->right = Delete(root->right,key);
     }
     else{
+        //if left tree is empty,return right tree
         if (root->left == NULL){
             Node* temp = root->right;
             delete root;
             return temp;
         }
+        //if right tree is empty,return left tree
         else if (root->right == NULL){
             Node* temp = root->left;
             delete root;
             return temp;
         }
+        //if node has left tree and right tree
+        //find the minest node in right tree,swap the node value,
+        //delete the swaped node in right tree
         Node* temp = FindMin(root->right);
         root->key = temp->key;
         root->right = Delete(root->right,temp->key);
     }
     return root;
 }
+//InOrder traversal 
 void  InorderTraversal(Node  *root){    
-      if(root  !=  NULL){    
+      if(root != NULL){    
                 InorderTraversal(root->left);    
                 cout  <<  root->key  <<  "  ";    
                 InorderTraversal(root->right);    
